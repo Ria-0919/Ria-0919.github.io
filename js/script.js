bbeb1cb0c53be0d19d6dfe563f31c974d4c24d20
Seroa <150595372+Ria-0919@users.noreply.github.com>
2024-09-30 03:01:50 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@ function openBook() {
     const book = document.getElementById("book");
     const content = document.querySelector(".portfolio-content");
     
-    // 책이 펼쳐지는 애니메이션 효과
     book.classList.add("open");
     
-    // 책이 펼쳐진 후 콘텐츠를 보여줌
     setTimeout(() => {
         book.style.display = "none"; // 책을 숨김
         content.classList.remove("hidden"); // 콘텐츠 표시
     }, 1000); // 1초 후에 콘텐츠를 표시
+}
+
+function showSection(section) {
+    // 모든 섹션 숨기기
+    const sections = document.querySelectorAll('.content > section');
+    sections.forEach(s => s.classList.add('hidden'));
+
+    // 클릭한 섹션 보여주기
+    document.getElementById(section).classList.remove('hidden');
 }
