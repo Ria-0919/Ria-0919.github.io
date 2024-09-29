c01c2d4732b7063cdb8ae812fd1016e5e55a39f1
Seroa <150595372+Ria-0919@users.noreply.github.com>
2024-09-30 03:38:53 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 function showSection(section) {
     const sections = document.querySelectorAll('.content');
-    sections.forEach(s => s.classList.remove('visible')); // 모든 섹션 숨기기
-
-    document.getElementById(section).classList.add('visible'); // 클릭한 섹션 보여주기
+    sections.forEach(s => s.classList.add('hidden')); // 모든 섹션 숨기기
+    document.getElementById(section).classList.remove('hidden'); // 클릭한 섹션 보여주기
 }
