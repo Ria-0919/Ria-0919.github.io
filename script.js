df6a20775d44a8cf9756598984147d5cba576c6d
Seroa <150595372+Ria-0919@users.noreply.github.com>
2024-09-30 02:37:13 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".nav-link").click(function(e) {
-        e.preventDefault();
-        const section = $(this).data("section");
-        $(".content").hide();
-        $("#" + section).fadeIn();
-    });
-});
