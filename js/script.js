f90f8c6e2ffceeff4a035f44303a8cf3a37bcdc2
Seroa <150595372+Ria-0919@users.noreply.github.com>
2024-09-30 03:52:46 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 function showSection(section) {
     const sections = document.querySelectorAll('.content');
-    sections.forEach(s => s.classList.add('hidden')); // 모든 섹션 숨기기
-    document.getElementById(section).classList.remove('hidden'); // 클릭한 섹션 보여주기
+    sections.forEach(s => s.classList.remove('visible')); // 모든 섹션 숨기기
+
+    document.getElementById(section).classList.add('visible'); // 클릭한 섹션 보여주기
 }
+
+// 처음 페이지 로드 시 프로필 섹션 보이기
+document.addEventListener('DOMContentLoaded', () => {
+    showSection('profile');
+});
