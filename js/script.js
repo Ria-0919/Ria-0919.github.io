9515768ec1e5c84db58f0aa227c065ca2442803d
Seroa <150595372+Ria-0919@users.noreply.github.com>
2024-09-30 03:25:45 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
-function openBook() {
-    const book = document.getElementById("book");
-    const content = document.querySelector(".portfolio-content");
-    
-    book.classList.add("open");
-    
-    setTimeout(() => {
-        book.style.display = "none"; // 책을 숨김
-        content.classList.remove("hidden"); // 콘텐츠 표시
-    }, 1000); // 1초 후에 콘텐츠를 표시
-}
-
 function showSection(section) {
-    const sections = document.querySelectorAll('.content > section');
-    sections.forEach(s => s.classList.add('hidden')); // 모든 섹션 숨기기
+    const sections = document.querySelectorAll('.content');
+    sections.forEach(s => s.classList.remove('visible')); // 모든 섹션 숨기기
 
-    document.getElementById(section).classList.remove('hidden'); // 클릭한 섹션 보여주기
+    document.getElementById(section).classList.add('visible'); // 클릭한 섹션 보여주기
 }
