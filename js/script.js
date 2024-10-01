e9112bf8f70d3239705150bf2c6a7c03eadf28b2
Seroa <150595372+Ria-0919@users.noreply.github.com>
2024-10-01 14:45:40 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,20 @@ const navButtons = document.querySelectorAll('.nav-btn');
 const scrollNav = document.getElementById('scroll-nav');
 
 window.addEventListener('scroll', () => {
-  scrollNav.style.display = window.scrollY > 50 ? 'flex' : 'none';
+    if (window.scrollY > 50) {
+        scrollNav.style.display = 'flex';
+    } else {
+        scrollNav.style.display = 'none';
+    }
 });
 
 navButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const targetId = button.getAttribute('data-target');
-    const targetElement = document.querySelector(targetId);
+    button.addEventListener('click', () => {
+        const targetId = button.getAttribute('data-target');
+        const targetElement = document.querySelector(targetId);
 
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
 });
