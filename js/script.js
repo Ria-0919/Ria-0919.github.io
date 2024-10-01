ebaa949b6affe527d26d4e57bb5b889b272f8312
Seroa <150595372+Ria-0919@users.noreply.github.com>
2024-10-01 14:27:36 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.nav-btn');
-    
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            const targetId = button.getAttribute('data-target');
-            const targetSection = document.querySelector(targetId);
-            if (targetSection) {
-                window.scrollTo({
-                    top: targetSection.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
+const navButtons = document.querySelectorAll('.nav-btn');
+const scrollNav = document.getElementById('scroll-nav');
 
-    window.addEventListener('scroll', function() {
-        const header = document.getElementById('scroll-nav');
-        if (window.scrollY > 50) {
-            header.style.display = 'block'; // 스크롤 시 헤더 나타나기
-        } else {
-            header.style.display = 'none'; // 스크롤 최상단 시 헤더 숨기기
-        }
-    });
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    scrollNav.style.display = 'flex';
+  } else {
+    scrollNav.style.display = 'none';
+  }
+});
+
+navButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const targetId = button.getAttribute('data-target');
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
 });
