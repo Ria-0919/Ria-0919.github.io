d68efe2d676a2b9c7556d9b19d072e563a1bf674
Seroa <150595372+Ria-0919@users.noreply.github.com>
2024-10-01 13:51:33 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll(".nav-btn");
-    
+    const header = document.getElementById("scroll-nav");
+
+    // 상단 네비게이션 표시/숨기기
+    window.onscroll = function() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            header.style.display = "block";
+        } else {
+            header.style.display = "none";
+        }
+    };
+
     buttons.forEach(button => {
         button.addEventListener("click", function() {
             const target = document.querySelector(this.dataset.target);
